c84c20d58f57284ab1f7196ce7b7a69e393b4abe
artur <arturdemaria7@gmail.com>
2023-01-17 16:13:02 -0300

    iniciando app em mobile e web/add backend

---
+++ web/src/components/Habit.tsx
@@ -0,0 +1,12 @@
+
+interface HabitProps {
+    completed: number
+}
+
+export function Habit(props: HabitProps) {
+    return (
+        <div className='bg-orange-400 w-10 h-10 text-white rounded m-2 text-center flex items-center justify-center'>
+            {props.completed}
+            </div>
+    )
+}
