a5326a2d173df347500d1f4ee9168a655864c801
artur <arturdemaria7@gmail.com>
2023-01-20 17:54:49 -0300

    popouts, modals & chart colors

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -25,14 +25,20 @@ export function SuummaryTable() {
             </div>
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summarydates.map(date => {
-                    return <HabitDay key={date.toString()}/>
+                    return (
+                        <HabitDay
+                            key={date.toString()}
+                            amount={5}
+                            completed={Math.round(Math.random() * 5)}
+                        />
+                    )
                 })}
 
-                {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, i) => {
+                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
                     return (
-                        <div 
-                        key={i} 
-                        className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+                        <div
+                            key={i}
+                            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
                     )
                 })}
 
