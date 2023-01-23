7dd6006864635c9915512471eefe8ea0357ff889
artur <arturdemaria7@gmail.com>
2023-01-23 17:47:58 -0300

    recreating '/mobile' directory

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import { ActivityIndicator, View } from "react-native";
+
+export function Loading() {
+    return (
+        <View style={{flex: 1, backgroundColor:'#09090A' ,justifyContent: 'center', alignItems: 'center'}}>
+            <ActivityIndicator color='#7C3AED'/>
+
+        </View>
+    );
+}
